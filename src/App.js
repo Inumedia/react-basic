74af3b19b7cbc710e35b5210da1b8c0d35f76055
Inumedia <inumedia@inumedia.net>
2017-01-09 21:06:25 -0500

    Adds basic Paginator functionality and related props

--- src/App.js
+++ src/App.js
@@ -6,32 +6,84 @@ import './App.css';
 
 class Paginator extends Component {
   static propTypes = {
-    range: PropTypes.number.isRequired,
+    itemCount: PropTypes.number.isRequired,
+    pageSize: PropTypes.number.isRequired,
+    selectedPage: PropTypes.number.isRequired,
+    maxPages: PropTypes.number.isRequired
   };
 
   render() {
-    const { range } = this.props;
+    const { selectedPage, itemCount, pageSize, maxPages } = this.props;
+
+    const previousPage = Math.max(selectedPage - 1, 0)
+    const totalPages = Math.ceil(itemCount / pageSize)
+    const nextPage = Math.floor(selectedPage + 1, totalPages)
+
+    var pages = [
+      1,
+      2
+    ];
+
+    pages[2] = <FontAwesome name="ellipsis-h" />
+
+    const middlePages = maxPages - 6
+    const startingMiddlePage = selectedPage - (middlePages / 2)
+    for(let page = 0; page < middlePages; ++page) {
+      pages[page + 3] = startingMiddlePage + page;
+    }
+
+    pages[middlePages + 3] = <FontAwesome name="ellipsis-h" />
+
+    for (let page = totalPages - 2; page < totalPages; ++page) {
+      pages[page] = page + 1;
+    }
 
     return (
       <div className="container">
+        <span onClick={this.changePage.bind(this, previousPage)}><FontAwesome name="arrow-left" /></span>
+        <span className="pages">
+          {pages.map(page => <b className={selectedPage === page ? 'activePage box' : 'inactivePage box'}>{page}</b>)}
+        </span>
+        <span onClick={this.changePage.bind(this, nextPage)}><FontAwesome name="arrow-right" /></span>
+        of {totalPages}
       </div>
     );
+  }
+
+  changePage(newPage) {
+    console.log(newPage)
+    this.props.changePage(newPage)
   }
 }
 
 
 class App extends Component {
   state = {
-    selectedPage: 1,
+    selectedPage: 20,
+    itemCount: 12345,
+    pageSize: 25,
+    maxPages: 12
   };
 
   render() {
+    const { selectedPage, itemCount, pageSize, maxPages } = this.state
     return (
       <div>
-        <div>{this.state.selectedPage}</div>
-        <Paginator />
+        <div>{selectedPage}</div>
+        <Paginator
+          changePage={this.changePage.bind(this)}
+          itemCount={itemCount}
+          pageSize={pageSize}
+          selectedPage={selectedPage}
+          maxPages={maxPages} />
       </div>
     );
+  }
+
+  changePage(newPage) {
+    this.setState({
+      selectedPage: newPage
+    })
   }
 }
 
